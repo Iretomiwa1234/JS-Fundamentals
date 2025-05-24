6d124afe8dd9ea619dd2c2e9c4db5beab9c4db93
DFS0507 <adeyemooluwatoyin2018@gmail.com>
2025-05-24 22:15:24 +0100

    concat-argument

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,3 @@
 const args = process.argv.slice(2);
 
-if (args[1] !== undefined) {
-  console.log(`${args[0]} is ${args[1]}`);
-} else if (args[0] !== undefined) {
-  console.log(args[0]);
-} else {
-  console.log();
-}
+console.log(`${args[0]} is ${args[1]}`);
