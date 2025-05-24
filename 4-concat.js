eb45137c205fb42c91812039daf9cb0819d77df0
DFS0507 <adeyemooluwatoyin2018@gmail.com>
2025-05-24 22:07:32 +0100

    concat with is

--- 4-concat.js
+++ 4-concat.js
@@ -1,11 +1,7 @@
-// Write a script that prints two arguments passed to it, in the following format: “ is ”
-
-// You must use console.log(...) to print all output
-// You are not allowed to use var
-
 const args = process.argv.slice(2);
-if (args.length < 2) {
+
+if (args[1] === undefined) {
   console.log(" is ");
-}else {
+} else {
   console.log(`${args[0]} is ${args[1]}`);
 }
